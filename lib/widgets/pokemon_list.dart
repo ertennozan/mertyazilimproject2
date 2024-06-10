91c9d541284a5475698389ab2c8d8caa86090317
ertennozan <ozanertenn@icloud.com>
2024-06-10 15:12:49 +0300

    description

---
+++ lib/widgets/pokemon_list.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:pokedex_app/model/pokemon_model.dart';
+import 'package:pokedex_app/services/pokedex_api.dart';
+import 'package:pokedex_app/widgets/pokelit_item.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class PokemonList extends StatefulWidget {
+   // ignore: prefer_const_constructors_in_immutables
+   PokemonList({Key? key}) : super(key: key);
+
+  @override
+  _PokemonListState createState() => _PokemonListState();
+}
+
+class _PokemonListState extends State<PokemonList> {
+  late Future<List<PokemonModel>> _pokemonListFuture;
+
+  @override
+  void initState() {
+    super.initState();
+    _pokemonListFuture = PokeApi.getPokemonData();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    debugPrint('pokemon List build calisti');
+    return FutureBuilder<List<PokemonModel>>(
+      future: _pokemonListFuture,
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          List<PokemonModel> _listem = snapshot.data!;
+
+          return GridView.builder(
+            itemCount: _listem.length,
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount:
+                      ScreenUtil().orientation == Orientation.portrait ? 2 : 3),
+              itemBuilder: (context, index) {
+                debugPrint('item builder calisti');
+                return PokeListItem(pokemon: _listem[index]);
+              });
+        } else if (snapshot.hasError) {
+          return const Center(
+            child: Text('Hata cikti'),
+          );
+        } else {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+      },
+    );
+  }
+}
